30d4ef10e21253c322daff2a8c9531ef206248f8
Connor Sundbeck <csundbeck04@gmail.com>
2018-02-13 22:47:37 -0600

    Pilot commit

---
+++ app/src/main/java/edu/txstate/crs181/billist/MainActivity.java
@@ -0,0 +1,26 @@
+package edu.txstate.crs181.billist;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button newBill = findViewById(R.id.btnNewBill);
+        Button myBillist = findViewById(R.id.btnMyBillist);
+
+        newBill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, NewBillActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/edu/txstate/crs181/billist/NewBillActivity.java
@@ -0,0 +1,13 @@
+package edu.txstate.crs181.billist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewBillActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_bill);
+    }
+}
