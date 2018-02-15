c44ca3db252b5c1dfb0ab7b47308584af2458b91
Connor Sundbeck <csundbeck04@gmail.com>
2018-02-14 19:47:55 -0600

    Set up for NewBillActivity.java
    Created variables for all fields/buttons
    Created clear button
    Changed text color to white

--- app/src/main/java/edu/txstate/crs181/billist/NewBillActivity.java
+++ app/src/main/java/edu/txstate/crs181/billist/NewBillActivity.java
@@ -2,6 +2,9 @@ package edu.txstate.crs181.billist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NewBillActivity extends AppCompatActivity {
 
@@ -9,5 +12,45 @@ public class NewBillActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_bill);
+
+        final EditText restaurantName = findViewById(R.id.txtRestaurantName);
+        final EditText billAmount = findViewById(R.id.txtBillAmount);
+        final EditText tipPercentage = findViewById(R.id.txtTipPercentage);
+        final EditText tipAmount = findViewById(R.id.txtTipAmount);
+        final EditText totalBill = findViewById(R.id.txtTotalBill);
+
+        Button saveBill = findViewById(R.id.btnSaveBill);
+        Button calculateBill = findViewById(R.id.btnCalculateBill);
+        Button clear = findViewById(R.id.btnClear);
+
+        //Save Bill button
+        saveBill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        //Calculate Button
+        calculateBill.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+        }
+    });
+
+        //Clear fields button
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                restaurantName.setText("");
+                billAmount.setText("");
+                tipPercentage.setText("");
+                tipAmount.setText("");
+                totalBill.setText("");
+            }
+
+
+        });
     }
 }
